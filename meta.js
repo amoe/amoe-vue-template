c16c854ec4857ad01c9cdcb41157fe5f3448ac2f
David Banks <amoebae@gmail.com>
2017-12-17 11:56:39 +0000

    more templating

--- meta.js
+++ meta.js
@@ -1,4 +1,13 @@
 // meta.js
 
 module.exports = {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name',
+    },
+    author: {
+      type: 'string',
+      message: 'Author',
+    },
 };
